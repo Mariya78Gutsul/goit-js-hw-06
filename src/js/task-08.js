fe9816856c936b7711032c0db5166e695cacd5a0
Mariya78Gutsul <mariyagutsui1@gmail.com>
2021-11-09 21:16:25 +0200

    выполнено 9 заданий

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onformSubmit);
 
+function onformSubmit(event){
+   event.prevenDefault();
+const mail = event.currentTarget.elements.email.value;
+const password = event.currentTarget.elements.password.value;
+if(!mail || !password) {
+   return alert("Please fill in all the fields!");
+}
+const formData ={
+   mail, password, 
+}
+console.log(formData);
+form.reset();
+}
